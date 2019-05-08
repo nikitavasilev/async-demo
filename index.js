c6f1a868b4dc05d89e0a990070b3d5e9c3a9c591
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-05-09 00:04:21 +0200

    callback

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 console.log('Before');
-setTimeout(() => {
-  console.log('Reading a user from a database...');
-}, 2000);
+// eslint-disable-next-line no-use-before-define
+getUser(1, user => {
+  // eslint-disable-next-line no-use-before-define
+  getRepositories(user.gitHubUsername, repositories => {
+    console.log('Repositories:', repositories);
+  });
+});
 console.log('After');
+
+// Callbacks
+// Promises
+// Async/await
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log('Reading a user from a database...');
+    callback({ id, gitHubUsername: 'mosh' });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Calling GitHub API...');
+    callback({ username, repositories: ['repo1', 'repo2', 'repo3'] });
+  }, 2000);
+}
