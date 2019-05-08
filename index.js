6038efcd97bb01b2fbc57dab45eea564c081d875
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-05-09 00:22:42 +0200

    callback hell

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 console.log('Before');
+
 // eslint-disable-next-line no-use-before-define
 getUser(1, user => {
   // eslint-disable-next-line no-use-before-define
   getRepositories(user.gitHubUsername, repositories => {
     console.log('Repositories:', repositories);
+    getCommits(repo, commits => {
+      // CALLBACK HELL
+    });
   });
 });
-console.log('After');
 
-// Callbacks
-// Promises
-// Async/await
+console.log('After');
 
 function getUser(id, callback) {
   setTimeout(() => {
@@ -24,4 +25,22 @@ function getRepositories(username, callback) {
     console.log('Calling GitHub API...');
     callback({ username, repositories: ['repo1', 'repo2', 'repo3'] });
   }, 2000);
+}
+
+// Solution to callback hell:
+
+console.log('Before');
+getUser(1, getRepositories);
+console.log('After');
+
+function getRepositories(user) {
+  getRepositories(user.gitHubUsername, getCommits);
+}
+
+function getCommits(repos) {
+  getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits) {
+  console.log(commits);
 }
